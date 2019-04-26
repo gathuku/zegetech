65582ccd3b90f2955bc39b15e622ecc149c80644
moses gathuku <mosesgathuku95@gmail.com>
2019-04-26 18:08:34 +0300

    added authentication method

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
+use Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
+       //validate Request
+       $rules=[
+         'name' => 'required',
+         'email' => 'required'
+       ];
 
-      return $request;
+       $validator=Validator::make($request->all(),$rules);
+
+       if ($validator->fails()) {
+         return $validator->messages();
+       }else{
+        // Attempt Auth
+            $auth= Auth::attempt(['email'=>$request['email'],'password'=>$request['password']]);
+            if ($auth) {
+              return 'Authenticated';
+            }else{
+              return ['error'=>'Credentials Dont match', 'code'=>'401'];
+            }
+
+       }
+    //  return $request;
     }
 }

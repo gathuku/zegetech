1ee4f71d00eb989d5744b08ff4dc29c2efc8c1b1
moses gathuku <mosesgathuku95@gmail.com>
2019-04-26 17:34:25 +0300

    added api_token on user model

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('/v1/login','API\AuthController@login');
+Route::prefix('v1')->namespace('API')->group(function(){
+
+      Route::post('/login','AuthController@login');
+      Route::post('/register','AuthController@register');
+});

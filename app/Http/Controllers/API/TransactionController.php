d5092ea6ed0c6d08432960ed5f6e6765936084de
moses gathuku <mosesgathuku95@gmail.com>
2019-04-27 10:23:31 +0300

    added tranfer feature

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -47,8 +47,43 @@ class TransactionController extends Controller
 
     }
 
-    public function transer()
+    public function transfer(Request $request)
     {
+
+        $token = $request->header('Authorization');
+        $tokenExplode=explode(" ",$token);
+        $theToken=$tokenExplode[1];
+
+        $amount=$request->amount;
+        $madeTo=$request->madeTo;
+
+        //return ['amount'=>$amount,'token'=>$theToken];
+        $userID=User::where('api_token',$theToken)->value('id');
+
+        if($userID){
+          //Get the current amount
+          $currentAmount=User::where('id',$userID)->value('wallet_amount');
+
+          $newAmount=$currentAmount - $amount;
+          //Update amount
+            $topUp= User::where('id',$userID)->update(['wallet_amount'=>$newAmount]);
+            //Update Notification
+            $saveTransaction=Transaction::create([
+              'made_by'=>$userID,
+              'made_to'=>$madeTo,
+              'type' =>'debit',
+              'amount'=>$amount,
+            ]);
+          if ($topUp && $saveTransaction) {
+
+            return ['message'=> 'Success Amount Sent'];
+
+          }else {
+            return ['message'=>'Amount could not be Sent'];
+          }
+        }else {
+          return ['message'=>'User not found'];
+        }
 
     }
 }
